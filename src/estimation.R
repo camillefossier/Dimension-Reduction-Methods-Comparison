037c694bd59143fdb6f4f37c6dc37c35228c09c1
camillefossier <camille.fossier@gmail.com>
2019-11-18 14:45:17 +0100

    generate 10D data (sensors) and estimating dimensionality (PCA and corrdim)

--- src/estimation.R
+++ src/estimation.R
@@ -1,8 +1,25 @@
+source("simulation.R")
+
 #### Dimensionality Estimation ####
+
+  # PCA : See examples/dimensionality_estimation.R (using prcomp for PCA)
 
   # Correlation dimension, Grassberger and Procaccia (case where q=2)
 
-  # PCA (ewww)
+corrDim <- function(data, epsilon = 10^seq(-2, 1, length.out = 100)){
+  distmat <- dist(data)
+  # Determine the proportion of distances <= epsilon
+  C <- numeric(length(epsilon))
+  for(k in seq_along(C)) {
+    C[k] <- sum(distmat <= epsilon[k]) #/ (nrow(X) - 1) / nrow(X)
+  }
+  
+  return(list(epsilon = epsilon, C = C))
+}
+
+derivate <- function(x, y, smoothing=1) {
+  return(c(diff(y, lag=smoothing) / diff(x, lag=smoothing), rep(NA, smoothing)))
+}
 
 #### Dimensionality Reduction ####
 
--- src/simulation.R
+++ src/simulation.R
@@ -1,4 +1,5 @@
 library(rgl)
+require(pdist)
 
 myColorRamp <- function(values, colors=c("red", "yellow", "green", "cyan", "blue")) {
   v <- (values - min(values))/diff(range(values))
@@ -37,4 +38,24 @@ sphere <- function(n, noise=0) {
   data = cbind(X,Y,Z,A)
   data = addNoise(data, noise)
   plot3d(data[,1], data[,2], data[,3], col=myColorRamp(data[,4]))
+}
+
+## 3-sensor data set ####
+## Artificial data as from P. Desmartines, PhD Tesis 1994
+generateData <- function(n) {
+  
+  # these sensors where selected randomly
+  sensors <- matrix(ncol = 3, data = 
+                      c(0.026, 0.236, -0.653, 0.310, 0.507, -0.270, -0.466,  -0.140, 0.353, -0.473,
+                        0.241, 0.193, 0.969, 0.094, 0.756, -0.978, -0.574, -0.502, -0.281, 0.993,
+                        0.026, -0.913, -0.700, 0.876, 0.216, -0.739, 0.556, -0.155, 0.431, 0.411))
+  
+  # draw random points on the 3d unit cube
+  unitcube <- matrix(runif(3 * n, -1, 1), ncol = 3)
+  
+  # We ode each point as the distance to sensors : intrinsic dimension = 3
+  # while extrinsic dimension = 10
+  X <- as.matrix(pdist(unitcube, sensors))
+  noise <- matrix(rnorm(ncol(X) * nrow(X), sd = .01), ncol = ncol(X))
+  return(X + noise)
 }
