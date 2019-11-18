0b2794d022f1a160119f9bbed561abadd9c6466d
camillefossier <camille.fossier@gmail.com>
2019-11-18 12:56:57 +0100

    Ideas for dimension reduction methods, methods proposed during the class

---
+++ src/estimation.R
@@ -0,0 +1,21 @@
+#### Dimensionality Estimation ####
+
+  # Correlation dimension, Grassberger and Procaccia (case where q=2)
+
+  # PCA (ewww)
+
+#### Dimensionality Reduction ####
+
+  # MDS : Multidimensional Scaling
+
+    # Metric MDS
+
+  # Sammon's Nonlinear Mapping
+
+  # Graph Distances
+
+    # Geodesic Graph Distance
+
+  # Data-driven lattice
+
+    # Locally linear embedding
