6d0739df55e8571eadce9eb761845de04ed3c480
camillefossier <camille.fossier@gmail.com>
2019-11-26 16:54:14 +0100

    finished trustworthiness but doesn't seem to return anything else than 0

--- src/evaluation.R
+++ src/evaluation.R
@@ -1,13 +1,24 @@
 trustworthiness <- function(data, manifold, k) {
   n = nrow(data)
   trust = 0
-  for (i in data) {
-    neighbors = NA # TODO
-    sort(neighbors) # TODO
-    for (n in neighbors) {
-      j = neighbors[n]
-      trust = trust + (n - k)
+  # distances des points à i en faible dimension
+  d_m = as.matrix(dist(manifold))
+  # distances des points à i en haute dimension
+  d_d = as.matrix(dist(data))
+  for (i in 1:n) {
+    d_m_i = d_m[,i]
+    d_d_i = d_d[,i]
+    # indices des points proches en faible dimension
+    o_m = order(d_m)
+    # indice des points proches en hautes dimensions
+    o_d = order(d_d)
+    u = setdiff(o_m[1:(k+1)], o_d[1:(k+1)])
+    if (length(u)>0) print(u)
+    for (j in u) {
+      
+      trust = trust + (j - k)
       
     }
   }
+  trust
 }
