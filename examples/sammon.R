66b881352a1e64184983cfe0ef032bcd24574498
camillefossier <camille.fossier@gmail.com>
2019-11-26 09:17:03 +0100

    sammon example for spi, sph, wav

---
+++ examples/sammon.R
@@ -0,0 +1,14 @@
+source("src/simulation.R")
+source("src/estimation.R")
+
+spi = spiral(n, p=2)
+flat = sammon_NLM(spi[,-4], 2)
+plot(flat$points[,1], flat$points[,2], col=myColorRamp(spi[,4]))
+
+sph = sphere(n)
+flat = sammon_NLM(sph[,-4], 2)
+plot(flat$points[,1], flat$points[,2], col=myColorRamp(sph[,4]))
+
+wav = waves(n)
+flat = sammon_NLM(wav[,-4], 2)
+plot(flat$points[,1], flat$points[,2], col=myColorRamp(wav[,4]))
