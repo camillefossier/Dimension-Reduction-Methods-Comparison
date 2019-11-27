4b76a6a601882a36bbd1420fc6024eeb0a1f39b8
camillefossier <camille.fossier@gmail.com>
2019-11-28 00:36:00 +0100

    better visualization and PCA

--- examples/full_simulation.R
+++ examples/full_simulation.R
@@ -7,23 +7,40 @@ n=1000
 spi = spiral(n, p=2, noise=2)
 plot3d(spi$data[,1], spi$data[,2], spi$data[,3], col=myColorRamp(spi$data[,4]), aspect=F)
 
+# Predict #
+
 mds = classical_MDS(spi$data[,-4], 2)
 sam = sammon_NLM(spi$data[,-4], 2)
 iso = isomap_ML(spi$data[,-4], 2, 7)
 lle = LLE_ML(spi$data[,-4], 2, 13)
+pca = predict(PCA(spi$data[,-4]), spi$data[,-4])
+
+high = spi$data[,-4]
+value = spi$data[,4]
 
 lows = list(
   mds=mds$points,
   sam=sam$points,
   iso=iso$points,
-  lle=lle$Y
+  lle=lle$Y,
+  pca=pca$coord[,1:2]
 )
+
 legend = c(
   "MDS",
   "Sammon",
   "Isomap",
-  "LLE"
+  "LLE",
+  "PCA"
 )
 
+# Plot #
+
+layout(matrix(c(1,2,3,4,5,6),2,3))
+
+for (l in seq_along(lows)) {
+  plot(lows[[l]][,1], lows[[l]][,2], col=myColorRamp(value), sub=legend[l], xlab="x", ylab="y")
+}
+
 K=seq.int(from=1, to=300, length.out=20)
-comp = compare(spi$data[,-4], lows, K, legend)
+comp = compare(high, lows, K, legend)
