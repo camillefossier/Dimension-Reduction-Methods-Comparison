66946185654e7aab2867457c85fb8396ee4bea26
Camille Fossier <cfossier@univ-lyon2.fr>
2019-11-28 14:59:07 +0100

    few changes

--- examples/full_simulation.R
+++ examples/full_simulation.R
@@ -11,7 +11,7 @@ full_comparison <- function(high, s, k_lle, k_iso, sig_kpca) {
   iso = isomap_ML(high, s, k_iso)
   lle = LLE_ML(high, s, k_lle)
   pca = PCA_ML(high, s)
-  kpc = KPCA_ML(high, s, sig)
+  kpc = KPCA_ML(high, s, sig_kpca)
   
   lows = list(
     mds=mds$points,
@@ -45,6 +45,8 @@ full_comparison <- function(high, s, k_lle, k_iso, sig_kpca) {
 
 n=1000
 
+# SPIRAL #
+
 spi = spiral(n, p=2)
 plot3d(spi$data[,1], spi$data[,2], spi$data[,3], col=myColorRamp(spi$data[,4]), aspect=F)
 f = full_comparison(spi$data[,-4], 2, k_iso=7, k_lle=13, sig_kpca=0.025)
@@ -52,5 +54,17 @@ f = full_comparison(spi$data[,-4], 2, k_iso=7, k_lle=13, sig_kpca=0.025)
 layout(matrix(c(1,2,3,4,5,6),2,3))
 for (l in seq_along(f$lows)) {
   plot(f$lows[[l]][,1], f$lows[[l]][,2], col=myColorRamp(spi$data[,4]), sub=f$legend[l], xlab="x", ylab="y")
+}
+plot_compare(f$comparison, f$legend)
+
+# SPHERE #
+
+sph = sphere(n)
+plot3d(sph$data[,1], sph$data[,2], sph$data[,3], col=myColorRamp(sph$data[,4]), aspect=F)
+f = full_comparison(sph$data[,-4], 2, k_iso=7, k_lle=13, sig_kpca=0.025)
+
+layout(matrix(c(1,2,3,4,5,6),2,3))
+for (l in seq_along(f$lows)) {
+  plot(f$lows[[l]][,1], f$lows[[l]][,2], col=myColorRamp(sph$data[,4]), sub=f$legend[l], xlab="x", ylab="y")
 }
 plot_compare(f$comparison, f$legend)
--- examples/kpca.R
+++ examples/kpca.R
@@ -3,14 +3,14 @@ source("src/estimation.R")
 
 n=1000
 
-spi = spiral(n, p=2)
+spi = spiral(n)
 sigmas=seq(from=0.00001, to=0.025, length.out=10)
 
-flats <- lapply(sigmas, function(sig) KPCA_ML(spi$data[,-4], sigma=sig))
+flats <- lapply(sigmas, function(sig) KPCA_ML(spi$data[,-4], 2, sigma=sig))
 
 layout(matrix(c(1:12), 3, 4))
 for (flat in flats) {
-  plot(rotated(flat), col=myColorRamp(spi$data[,4]))
+  plot((flat), col=myColorRamp(spi$data[,4]))
 }
-comp = compare(spi$data[,-4], lapply(flats, rotated), seq(1,300,20))
-plot_compare(comp, legend=sig)
+comp = compare(spi$data[,-4], flats, seq(1,300,20))
+plot_compare(comp, legend=sigmas)
