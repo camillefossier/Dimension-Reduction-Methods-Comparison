d0bdeeecad6a3eac7cf94300ccd4cb8187e998eb
camillefossier <camille.fossier@gmail.com>
2019-12-01 18:55:58 +0100

    few changes

--- examples/full_simulation.R
+++ examples/full_simulation.R
@@ -43,13 +43,13 @@ full_comparison <- function(high, s, k_lle, k_iso, sig_kpca, trust_K = seq.int(f
   )
 }
 
-n=1000
+n=100
 
 # SPIRAL #
 
 spi = spiral(n, p=2)
 plot3d(spi$data[,1], spi$data[,2], spi$data[,3], col=myColorRamp(spi$data[,4]), aspect=F)
-f = full_comparison(spi$data[,-4], 2, k_iso=7, k_lle=13, sig_kpca=0.025)
+f = full_comparison(spi$data[,-4], 2, k_iso=7, k_lle=13, sig_kpca=0.025, trust_K=seq(from=1, to=300, length.out=20))
 
 layout(matrix(c(1,2,3,4,5,6),2,3))
 for (l in seq_along(f$lows)) {
@@ -61,7 +61,7 @@ plot_compare(f$comparison, f$legend)
 
 sph = sphere(n)
 plot3d(sph$data[,1], sph$data[,2], sph$data[,3], col=myColorRamp(sph$data[,4]), aspect=F)
-f = full_comparison(sph$data[,-4], 2, k_iso=7, k_lle=13, sig_kpca=0.025)
+f = full_comparison(sph$data[,-4], 2, k_iso=7, k_lle=13, sig_kpca=0.025, trust_K=seq(from=1, to=300, length.out=20))
 
 layout(matrix(c(1,2,3,4,5,6),2,3))
 for (l in seq_along(f$lows)) {
@@ -73,12 +73,18 @@ plot_compare(f$comparison, f$legend)
 
 wav = waves(n)
 plot3d(wav$data[,1], wav$data[,2], wav$data[,3], col=myColorRamp(wav$data[,4]), aspect=F)
-f = full_comparison(wav$data[,-4], 2, k_iso=7, k_lle=13, sig_kpca=0.025)
+f = full_comparison(wav$data[,-4], 2, k_iso=7, k_lle=13, sig_kpca=0.025, trust_K=seq(from=1, to=300, length.out=20))
 
 layout(matrix(c(1,2,3,4,5,6),2,3))
 for (l in seq_along(f$lows)) {
   plot(f$lows[[l]][,1], f$lows[[l]][,2], col=myColorRamp(wav$data[,4]), sub=f$legend[l], xlab="x", ylab="y")
 }
+plot_compare(f$comparison, f$legend)
+
+# 10D data #
+
+ten = data_10D(n)
+f = full_comparison(ten$data, 3, k_iso=20, k_lle=12, sig_kpca=0.025, trust_K=seq(from=1, to=300, length.out=20))
 plot_compare(f$comparison, f$legend)
 
 # FACES #
@@ -103,6 +109,12 @@ remove_doubles <- function(mat) {
   mat[-to_remove,]
 }
 
+layout(matrix(1:12,3,4))
+for (i in sample(list.files(folder), 12, replace = F)) {
+  im = t(readJPEG(paste(c(folder, i), collapse="/")))
+  image(im[,ncol(im):1], col=gray.colors(256, start=0, end=1))
+}
+
 images = unlist(lapply(list.files(folder), function(i) {
   img = flatten(readJPEG(paste(c(folder, i), collapse="/")))
   complete = max(0, 128*128 - length(img))
@@ -111,6 +123,5 @@ images = unlist(lapply(list.files(folder), function(i) {
 images = matrix(images, nrow=length(list.files(folder)))
 
 images=remove_doubles(images)
-f = full_comparison(images, 5, k_iso=20, k_lle=12, sig_kpca=1e-4, trust_K=seq(from=1, to=500, length.out=20))
-comp = compare(images, f$lows, seq(from=1, to=300, length.out=20))
-plot_compare(comp, f$legend)
+f = full_comparison(images, 5, k_iso=20, k_lle=12, sig_kpca=1e-4, trust_K=seq(from=1, to=200, length.out=20))
+plot_compare(f$comparison, f$legend)
